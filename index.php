1b754d51a7bc32322f8a89c4294d8ef9a864548d
dodungvts <dodungvts@gmail.com>
2025-11-17 12:32:12 +0700

    Add index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><title>Email Tracker - Render.com</title></head>
+<body style="font-family:Arial;text-align:center;padding:50px;">
+  <h1>Email Tracking Active</h1>
+  <p><a href="log.txt">Xem log mở email</a></p>
+  <p><small>Font URL: <code><?php echo $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST']; ?>/track.php</code></small></p>
+</body>
+</html>
